48943b8456ae59f17fbde192cfbd9146925bb120
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-12-22 10:49:20 -0500

    fixing say Hi bug

--- app.py
+++ app.py
@@ -54,14 +54,14 @@ def processRequest(req):
 
     if req.get("result").get("action") == "sayHi":
         result = req.get("result")
-        originalRequest = req.get("originalRequest")
-        device = originalRequest.get("device")
-        formatted_address = device.get("formatted_address")
+        # originalRequest = req.get("originalRequest")
+        # device = originalRequest.get("device")
+        # formatted_address = device.get("formatted_address")
         parameters = result.get("parameters")
         name = parameters.get("given-name")
         return{
-            "speech": 'Hey' + name + ' its been such a amazing experience learning Machine learning, creating cool bots, writing API for backend , server deployments, continuous integation and python. thanks for showing the way... giving me a why!!' + formatted_address ,
-            "displayText":  'Hey' + name + 'its been such a amazing experience learning Machine learning, creating cool bots, writing API for backend , server deployments, continuous integation and python. thanks for showing the way... giving me a why!!' + formatted_address,
+            "speech": 'Hey' + name + ' its been such a amazing experience learning Machine learning, creating cool bots, writing API for backend , server deployments, continuous integation and python. thanks for showing the way... giving me a why!!'  ,
+            "displayText":  'Hey' + name + 'its been such a amazing experience learning Machine learning, creating cool bots, writing API for backend , server deployments, continuous integation and python. thanks for showing the way... giving me a why!!',
             # "data": data,
             # "contextOut": [],
             "source": "apiai-weather-webhook-sample"
